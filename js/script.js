50b5e8041f8e46a3a6ca42a4e7175a3aa8b0b495
Jesper Mathiassen <jm@bellcom.dk>
2012-04-13 15:45:52 +0200

    Misc changes

---
+++ js/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function () {
+	var slideNavItems = jQuery('.custom-slideshow .item-bg');
+	slideNavItems.each(function(k,v) {
+		jQuery(v).parent().parent().parent().css('background-image','url('+jQuery(v).attr("title")+')');
+	});
+
+	jQuery('.custom-slideshow').mouseover(function() {
+		jQuery('.custom-slideshow .active').addClass('hover', 500);
+		jQuery('#views_slideshow_cycle_main_rotating_image_view-block').addClass('hover', 500);
+	}).mouseout(function() {
+		jQuery('.custom-slideshow .hover').removeClass('hover', 500);
+	});
+
+	jQuery('.view-id-newslist ul li').not(':last-child').each(function(k,v) {
+		jQuery(v).find('.news-list-next').html(jQuery(v).next().find('.news-list-right').html());
+	});
+});
