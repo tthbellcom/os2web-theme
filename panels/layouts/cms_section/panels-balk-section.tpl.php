37af4629af44149e8e9a0ef9623bebf21b64aaf5
Jesper Mathiassen <jm@bellcom.dk>
2012-04-16 10:15:17 +0200

    Theme updates

--- panels/layouts/cms_section/panels-balk-section.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="panel-display container-16 panel-section" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="panel-panel grid-13 alpha omega panel-region-lead">
-    <div class="inside"><?php print $content['lead']; ?></div>
-  </div>
-
-  <div class="panel-panel grid-13 alpha omega panel-region-content">
-    <div class="inside"><?php print $content['content']; ?></div>
-  </div>
-</div>
