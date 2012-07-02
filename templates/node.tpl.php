9eceb9a12723c35b63086a44275d47a1e733307d
Franz Skaaning <fs@bellcom.dk>
2012-07-02 11:48:37 +0200

    fs

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -15,7 +15,7 @@
       <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
     <?php endif; ?>
 <?php if ($view_mode != 'meeting_detail_view' && arg(0) == 'node' && is_numeric(arg(1))): $nodeid = arg(1); ?>
-<div class="printvenlig-side"><a href="/print/<?php print $nodeid; ?>"></a></div>
+<div class="printvenlig-side"><a href="/print/<?php print $nodeid; ?>">&nbsp;</a></div>
 <?php endif; ?> 
 
   <?php // print render($title_suffix); ?>
