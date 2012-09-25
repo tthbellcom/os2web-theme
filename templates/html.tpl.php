7aa3f91139a23acbf323dad654950ea73e9f8f61
Morten Hansen <mmh@bellcom.dk>
2012-09-25 12:43:04 +0200

    add siteimprove js

---
+++ templates/html.tpl.php
@@ -0,0 +1,27 @@
+<?php print $doctype; ?>
+<html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
+<head<?php print $rdf->profile; ?>>
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>  
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body<?php print $attributes;?>>
+  <div id="skip-link">
+    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  </div>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+<script type="text/javascript">
+/*<![CDATA[*/
+(function() {
+var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+sz.src = '//ssl.siteimprove.com/js/siteanalyze_1480.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
+/*]]>*/
+</script>
+</body>
+</html>
