045a71e9173d4d21928c7c4d17774f9552e1f16e
Thomas Thune Hansen <tth@bellcom.dk>
2012-11-08 10:24:49 +0100

    fix døde links på dagsordensiden visning

--- js/jquery.dagsorden.js
+++ js/jquery.dagsorden.js
@@ -1,16 +1,12 @@
 (function ($) {
 var is_visible = false;
- 
-$(".agenda-bullet-list").attr("href", "#");
 
 $('.agenda-item').hide(); 
 
-$('a.agenda-bullet-list').click(function() {
-
-$(this).next('.agenda-item').toggle();
-$(this).toggleClass('open')
-
-return false;
- 
-});
+$('a.agenda-bullet-list').click(function(e) {
+  e.preventDefault();
+  $(this).next('.agenda-item').toggle();
+  $(this).toggleClass('open')
+  return false;
+  });
 }(jQuery));
