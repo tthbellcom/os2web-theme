2b4ff03a5fa6b36db676507f52491a77182bef3e
Franz Skaaning <fs@bellcom.dk>
2013-01-07 10:55:06 +0100

    rettet fejl i stack

--- panels/layouts/os2web_3stack/panels-os2web-3stack.tpl.php
+++ panels/layouts/os2web_3stack/panels-os2web-3stack.tpl.php
@@ -1,15 +1,15 @@
 <div class="panel-display panel-node clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   
   <div class="panel-panel panel-region-stack1">
-    <div class="inside"><?php print $content['kol1']; ?></div>
+    <div class="inside"><?php print $content['stack1']; ?></div>
   </div>
 
   <div class="panel-panel panel-region-stack2">
-    <div class="inside"><?php print $content['kol2']; ?></div>
+    <div class="inside"><?php print $content['stack2']; ?></div>
   </div>
 
   <div class="panel-panel panel-region-stack3">
-    <div class="inside"><?php print $content['kol3']; ?></div>
+    <div class="inside"><?php print $content['stack3']; ?></div>
   </div>
 
 </div>
