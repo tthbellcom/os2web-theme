9e2a8d6281c515d0a39c254df725d191b821dd3a
Franz Skaaning <fs@bellcom.dk>
2013-01-08 12:08:11 +0100

    Layouts til Odsherred

---
+++ panels/layouts/os2web_2col-1-3/panels-os2web-2col-1-3.tpl.php
@@ -0,0 +1,11 @@
+<div class="panel-display 2col-1-3 clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  
+  <div class="panel-panel panel-region-col1">
+    <div class="inside"><?php print $content['col1']; ?></div>
+  </div>
+
+  <div class="panel-panel panel-region-col2">
+    <div class="inside"><?php print $content['col2']; ?></div>
+  </div>
+
+</div>
---
+++ panels/layouts/os2web_forside-5-7/panels-os2web-forside-5-7.tpl.php
@@ -0,0 +1,16 @@
+<div class="panel-display panel-node clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-top-container clearfix">
+    <div class="panel-panel grid-5 alpha panel-region-top-left">
+      <div class="inside"><?php print $content['top_left']; ?></div>
+    </div>
+
+    <div class="panel-panel grid-7 omega panel-region-top-right">
+      <div class="inside"><?php print $content['top_right']; ?></div>
+    </div>
+  </div>
+
+  <div class="panel-panel grid-12 alpha omega panel-region-bottom">
+    <div class="inside"><?php print $content['bottom']; ?></div>
+  </div>
+</div>
+
