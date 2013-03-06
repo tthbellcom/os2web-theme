a6f765a9247ef1479a72575990864ccae1ae7a26
Jesper Mathiassen <jm@bellcom.dk>
2013-03-06 17:55:29 +0100

    cleanups

--- template.php
+++ template.php
@@ -5,24 +5,24 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
- 
- function cmstheme_preprocess_html(&$variables) {
+function os2web_theme_preprocess_html(&$variables) {
   $theme_path = path_to_theme();
   drupal_add_js($theme_path . '/js/script.js');
   drupal_add_js($theme_path . '/js/jquery.vegas.js');
   drupal_add_js($theme_path . '/js/jquery.phonenumber.js');
-  drupal_add_js($theme_path . '/js/jquery.dagsorden.js', array('scope' => 'footer', 'weight' => 5));
+  drupal_add_js($theme_path . '/js/jquery.dagsorden.js', array('scope' => 'footer',
+    'weight' => 5));
 }
 
 /**
  * Implements hook_menu_link().
  */
-function cmstheme_menu_link(array $variables) {
+function os2web_theme_menu_link(array $variables) {
   $element = $variables['element'];
   $sub_menu = '';
 
@@ -33,7 +33,8 @@ function cmstheme_menu_link(array $variables) {
   if ($element['#below']) {
     $sub_menu = drupal_render($element['#below']);
   }
-  $output = l($element['#title'], $element['#href'], array('attributes' => array('title' => $element['#title'])));
+  $output = l($element['#title'], $element['#href'], array('attributes' => array(
+      'title' => $element['#title'])));
   return '<li' . drupal_attributes($element['#attributes']) . '>' . $output . $sub_menu . "</li>\n";
 }
 
@@ -42,7 +43,7 @@ function cmstheme_menu_link(array $variables) {
  *
  * tth@bellcom.dk check if there is a better way to do this...
  */
-function cmstheme_breadcrumb($variables) {
+function os2web_theme_breadcrumb($variables) {
   $breadcrumb = $variables['breadcrumb'];
 
   $nid = arg(1);
@@ -63,8 +64,10 @@ function cmstheme_breadcrumb($variables) {
     if (isset($node) && is_object($node) && $node->type == 'meeting') {
       unset($breadcrumb);
       $breadcrumb[0] = l(t('Forside'), '<front>', array('attributes' => array('title' => 'Forside')));
-      $breadcrumb[] = l(t('Politik & planer'), 'politik-og-planer', array('attributes' => array('title' => 'Politik og planer')));
-      $breadcrumb[] = l(t('Søg i dagsordener og referater'), 'dagsorden-og-referat', array('attributes' => array('title' => 'Søg i dagsordner og referater')));
+      $breadcrumb[] = l(t('Politik & planer'), 'politik-og-planer', array('attributes' => array(
+          'title' => 'Politik og planer')));
+      $breadcrumb[] = l(t('Søg i dagsordener og referater'), 'dagsorden-og-referat', array(
+        'attributes' => array('title' => 'Søg i dagsordner og referater')));
       $breadcrumb[] = l(t($title), '#');
     }
     $output .= '<div class="breadcrumb">' . implode('<div class="bread-crumb"> &gt; </div> ', $breadcrumb) . '</div>';
@@ -75,7 +78,7 @@ function cmstheme_breadcrumb($variables) {
 /**
  * Implements hook_preprocess_region().
  */
-function cmstheme_preprocess_region(&$vars) {
+function os2web_theme_preprocess_region(&$vars) {
   global $user;
   if ($vars['region'] === 'sidebar_first') {
     $dirty = FALSE;
@@ -106,7 +109,7 @@ function cmstheme_preprocess_region(&$vars) {
  *
  * tekst i søgefelt
  */
-function cmstheme_form_alter(&$form, &$form_state, $form_id) {
+function os2web_theme_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
     $form['search_block_form']['#attributes']['placeholder'] = t('søg på syddjurs.dk');
   }
@@ -122,11 +125,10 @@ function cmstheme_form_alter(&$form, &$form_state, $form_id) {
 /**
  * Implements hook_file_field_item().
  */
-function cmstheme_filefield_item($file, $field) {
+function os2web_theme_filefield_item($file, $field) {
   if (filefield_view_access($field['field_name']) && filefield_file_listed($file, $field)) {
     // Default theming.
     return theme('filefield_file', $file);
   }
   return '';
 }
-?>
